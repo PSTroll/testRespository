95c902d03093a5605dc70efe6fe3ac0d34e88bd3
PSTroll <pstrollyt@gmail.com>
2022-08-21 09:26:09 +0200

    pasteCode

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const allUsers = ['Martyna', 'Marcin', 'Dominika', 'Oliwia', 'Wojtek', 'Stefan'];
+
+class App extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            filterUsers: allUsers
+        }
+    }
+
+    filterUsers(e) {
+        const text = e.target.value;
+        const filterUsers = this.getFilteredUsersFromText(text);
+        this.setState({filterUsers})
+    }
+
+    getFilteredUsersFromText(text) {
+        return allUsers.filter(user => user.toLowerCase().includes(text.toLowerCase()));
+    }
+
+    render() {
+        return (
+            <div>
+                <input type = 'text' placeholder = 'Filtruj imię' onChange = {this.filterUsers.bind(this)} />
+                <UserList users = {this.state.filterUsers} />
+            </div>
+        )
+    }
+
 }
+
+const UserList = ({ users }) => {
+    if(users.length > 0){
+        return (
+            <ul>
+                {users.map(user => <li key={user}>{user}</li>)}
+            </ul>
+        )
+    }
+    return 'No results';
+}
+
+ReactDOM.render(<App/>, document.getElementById('root'));
 
 export default App;
